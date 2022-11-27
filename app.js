8ec1dcaff32c39005f6e9386137fbcb07673a11a
Rishu <rishut996@gmail.com>
2022-11-27 15:02:26 +0530

    port number displayed

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.post('/contact', (req, res) => {
 
 // START THE SERVER
 app.listen(process.env.PORT||8000, () => {
-    console.log(`The application started successfully on port`);
+    console.log(`The application started successfully on PORT : ${port}`);
 });
 
 
